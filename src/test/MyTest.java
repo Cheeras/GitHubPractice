12caef68a37da0e6a158888b0503e42234cbc98e
Cheeras <shankarcheerala@gmail.com>
2026-07-15 15:54:00 +0530

    update MyTest file

--- src/test/MyTest.java
+++ src/test/MyTest.java
@@ -3,5 +3,9 @@ package test;
 public class MyTest {
     public static void main(String[] args) {
         System.out.println("adding myTest");
+        String name = "MyTest";
+        int age = 20;
+        String city = "Seattle";
+        System.out.println(name +" " + age +" "+city);
     }
 }
