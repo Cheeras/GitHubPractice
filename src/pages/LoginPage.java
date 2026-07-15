ed011473c079aa61b1b59bd1ccbdf9c361caf7d6
Cheeras <shankarcheerala@gmail.com>
2026-07-15 11:42:56 +0530

    Second git commit

--- src/pages/LoginPage.java
+++ src/pages/LoginPage.java
@@ -4,5 +4,9 @@ public class LoginPage {
     
     public static void main(String[] args) {
         System.out.println("Hello LoginPage");
+        String username = "shankar";
+        String password = "shankar@123";
+        System.out.println(username +" "+password);
+        System.out.println("Login in successful");
     }
 }
---
+++ src/pages/SearchPage.java
@@ -0,0 +1,6 @@
+package pages;
+public class SearchPage {
+ public static void main(String[] args) {
+    System.out.println("from Search Page");
+ }
+}
