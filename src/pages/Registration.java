ce36ccd07ef5d2e73929c56dd5ccdff5fdc7e0c6
Cheeras <shankarcheerala@gmail.com>
2026-07-15 15:39:13 +0530

    Added new feature Registraion

---
+++ src/pages/Registration.java
@@ -0,0 +1,8 @@
+package pages;
+
+public class Registration {
+ public static void main(String[] args) {
+    String name = "registration";
+    System.out.println(name);
+ } 
+}
---
+++ src/test/RegistrationTest.java
@@ -0,0 +1,7 @@
+package test;
+
+public class RegistrationTest {
+    public static void main(String[] args) {
+        
+    }
+}
