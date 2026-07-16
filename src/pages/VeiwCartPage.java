703657c578ee70bfd205ab892cd8c75f2251c550
Cheeras <shankarcheerala@gmail.com>
2026-07-16 10:17:41 +0530

    Added ViewCartPage changes

---
+++ src/pages/VeiwCartPage.java
@@ -0,0 +1,9 @@
+package pages;
+
+public class VeiwCartPage {
+    
+    public static void main(String[] args) {
+        System.out.println(" I am from view cart page");
+        System.out.println("Items added successfully in View Cart page");
+    }
+}
