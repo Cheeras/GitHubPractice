cbb1e4bd0caa617e4afd0869521f72f6b5208075
Cheeras <shankarcheerala@gmail.com>
2026-07-16 10:23:30 +0530

    updated cartPage

--- src/pages/CartPage.java
+++ src/pages/CartPage.java
@@ -3,5 +3,6 @@ package pages;
 public class CartPage{
     public static void main(String[] args) {
         System.out.println("from Cart Page");
+        System.out.println("Updated cart page");
     }
 }
