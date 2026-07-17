5d74bd0e746c86e9797fd8a1421ee34016cb8002
Sravyacheeras <pusulurisravya@gmail.com>
2026-07-17 16:35:35 +0530

    Updated checkout as per Cheeras review comments

--- src/pages/CheckOutPage.java
+++ src/pages/CheckOutPage.java
@@ -2,4 +2,7 @@ public class CheckOutPage {
     public void doCheckOut(){
         System.out.println("doCheckoutPage");
     }
+    public void fillShippingForm(){
+        System.out.println("Please fill the shipping form")
+    }
 }
